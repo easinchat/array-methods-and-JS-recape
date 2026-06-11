e661bb132822db4257a02c0ddbb3b049b73f404a
easinchat <easin.chat@email.com>
2026-06-11 20:17:44 +0600

    first commit array using map

---
+++ array-methods.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    name: "Redmi",
+    brand: "Xiaomi",
+    price: 30000,
+    color: "black",
+  },
+  {
+    name: "Nokia",
+    brand: "Nokia",
+    price: 7000,
+    color: "white",
+  },
+  {
+    name: "Samsung",
+    brand: "Samsung",
+    price: 13000,
+    color: "blue",
+  },
+];
+const result = products.map((product) => product.price);
+console.log(result);
