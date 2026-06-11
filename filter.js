80791433d1ca5707ce0eb820089b4b8e3141f59f
easinchat <easin.chat@email.com>
2026-06-11 20:37:32 +0600

    using filter

---
+++ filter.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    name: "Redmi",
+    brand: "Xiaomi",
+    price: 30000,
+    color: "black",
+  },
+  {
+    name: "Nokia",
+    brand: "Nokia",
+    price: 7000,
+    color: "white",
+  },
+  {
+    name: "Samsung",
+    brand: "Samsung",
+    price: 13000,
+    color: "blue",
+  },
+];
+
+const result = products.filter((product) => product.price >= 8000);
+console.log(result);
